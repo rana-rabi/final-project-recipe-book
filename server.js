ab8a69b2b9cc1f9ddf8dfd804db832dab2ae7ccd
Rana Rabie <ranarabia132@gmail.com>
2026-07-16 13:33:53 +0300

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const dns = require('dns');
+const cors = require('cors');
 dns.setServers(['1.1.1.1', '8.8.8.8']);
 const path = require('path');
 const express = require('express');
@@ -12,9 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/recipeBookDB';
 
-app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+app.use(express.json());
+app.use(cors()); 
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/recipes', recipeRoutes);
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
